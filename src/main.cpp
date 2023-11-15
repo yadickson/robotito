79a03876567992f5fc036f897ba0166f1340f3a2
Yadickson Soto <yadickson@gmail.com>
2023-11-15 14:36:48 -0300

    Add new structure

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <ncurses.h>
+#include <clocale>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "");
+
+    initscr();
+    noecho();
+    cbreak();
+    refresh();
+
+    WINDOW * window  = newwin(0, 0, 0, 0);
+    
+    box(window, 0, 0);
+    mvwaddstr(window, 1, 1, "\U0001F600");
+    wrefresh(window);
+
+    getch();
+    endwin();
+
+
+  return 0;
+}
+
